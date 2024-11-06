f3bebc67325db1a78dc182ea78b733d47a6d721d
EnzoBaldinotti <enzo.baldinotti@unifesp.br>
2024-11-05 21:02:17 -0300

    Update Vizualização_ponteiros.c

--- exercícios/testes /Vizualização_ponteiros.c
+++ exercícios/testes /Vizualização_ponteiros.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+    int main(){
+        int x=1,y=3 ;
+        int *px=&y, *py=&y;
+        *px=*px+*py;
+ 
+        printf("x=%d, y=%d\n", x, y);
+        printf("*px=%d, *py=%d\n", *px, *py);
+        printf("px=%p, py=%p\n", px, py);
+        printf("&x=%p, &y=%p\n", &x, &y);
+        printf("&x=%p, &y=%p\n", x, y);  
+        printf("y=%d, x=%d\n", y, x);
+    return 0;
+} 
