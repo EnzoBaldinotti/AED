ed4bc16948351d0f8410a3128f8c1dc29f9a5ca5
EnzoBaldinotti <enzo.baldinotti@unifesp.br>
2024-11-05 21:03:27 -0300

    Create Sudoku Hard.c

---
+++ exercícios/testes /Sudoku Hard.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void preencher_sudoku (int M[3][3][3][3]) {
+  int i, j, k, l;
+  for (i = 0; i < 3; i++) {
+    for (k = 0; k < 3; k++) {
+      for (j = 0; j < 3; j++) {
+        for (l = 0; l < 3; l++) {
+          scanf("%d", &M[i][j][k][l]);
+        }
+      }
+    }
+  }
+}
+
+void print_sudoku(int matriz[3][3][3][3]) {
+  int i, j, k, l;
+  int n;
+  rand(time(0));
+
+  n=rand%10;
+  for(i=0; i<3; i++) {
+    for(k=0; k<3; k++) {
+      for(j=0; j<3; j++) {
+        for(l=0; l<3; l++) {
+          printf("[%d] ", matriz[i][j][k][l]);
+        }
+        if(j<2) printf("| ");
+      }
+      printf("\n");
+    }
+    if(i<2) printf("---------------------------------------\n");
+  }
+}
+
+
+
+int main() {
+  int matriz[3][3][3][3];
+
+  preencher_sudoku(matriz);
+  print_sudoku(matriz);
+
+}
