c383b575e246cc167f9d6641cbf13ec65f7ab295
EnzoBaldinotti <enzo.baldinotti@unifesp.br>
2024-11-12 15:00:11 -0300

    Create 4-areaCirculo 1002.c

---
+++ exercícios/4-areaCirculo 1002.c
@@ -0,0 +1,12 @@
+//Enzo Baldinotti Ravanelli RA:176265//
+#include <stdio.h>
+
+int main(){
+    double n;
+    double conta;
+    scanf("%lf", &n);
+    conta = 3.14159 * n * n;
+    printf("A=%.4lf\n", conta);
+
+    return 0;  
+}
