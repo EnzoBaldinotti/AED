5fa96aa34041992aeaa307e3c3d6b1062b6f05b7
EnzoBaldinotti <enzo.baldinotti@unifesp.br>
2025-01-21 14:00:04 -0300

    Create 116 basics 1001.c

---
+++ exercícios/116 basics 1001.c
@@ -0,0 +1,13 @@
+//Enzo Baldinotti Ravanelli//
+#include <stdio.h>
+
+int main(void){
+    int x,y;
+    scanf("%d", &x);
+    scanf("%d", &y);
+    
+    int tot = x+y;
+    printf("X = %d\n");
+    
+    return 0;
+}
