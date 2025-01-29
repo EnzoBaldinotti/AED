55793d4a801f1487c3e81a1ca68e043558e9497f
EnzoBaldinotti <enzo.baldinotti@unifesp.br>
2025-01-28 22:50:24 -0300

    Create 124 vagões 1162.c

---
+++ exercícios/124 vagões 1162.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#define max 50
+
+int diff(int a, int b){
+    
+    return a - b;
+}
+
+void troca(int *a, int *b){
+    
+    int aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+int insertionSort(int *V, int n){
+    int trocas = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        int j = i, k = j - 1;
+
+        while (k > -1 && comp(V[j], V[k]) < 0)
+        {
+            troca(&V[j], &V[k]);
+            ++trocas;
+            j--, 
+            k--;
+        }
+    }
+
+    return trocas;
+}
+
+int main()
+{
+    int N, L, v[max];
+
+    scanf("%d", &N);
+
+    for (int k = 0; k < N; k++) {
+        
+        scanf("%d", &L);
+
+        for (int i = 0; i < L; i++){
+            
+            scanf("%d", &v[i]);
+        }
+
+        printf("Optimal train swapping takes %d swaps.\n", insertionSort(v, L));
+    }
+
+    return 0;
+}
